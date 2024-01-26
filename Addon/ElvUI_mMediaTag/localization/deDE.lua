8817cdea52e03ab26fdd37216a7fcac291e2ca8c
Dlarge <dlarge500@googlemail.com>
2024-01-26 16:23:31 +0100

    add start Localization Support

---
+++ Addon/ElvUI_mMediaTag/localization/deDE.lua
@@ -0,0 +1,76 @@
+﻿local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "deDE") -- German localization by Dlarge
+if not L then return end
+
+--Tree--
+L["General"] = "Allgemein"
+L["About"] = "Über"
+L["Misc"] = "Sonstiges"
+L["Castbar"] = "Zauberleiste"
+L["Changelog"] = "Änderungen"
+
+--General--
+L["Welcome text"] = "Wilkommenstext"
+L["Roll Button"] = "Rolltaste"
+L["Chat Button"] = "Chattaste"
+L["Keystone to Chat"] = "Schlüsselstein in Chat"
+L["Show Keystone to Chat"] = "Zeige Schlüsselstein in Chat"
+L["Instance Difficulty"] = "Instanz Schwierigkeit"
+L["AFK Screen"] = "AFK Bildschirm"
+L["Show Welcome text"] = "Zeige Willkommenstext"
+L["Color Normal"] = "Farbe Normal"
+L["Color Hover"] = "Farbe Hover"
+L["Color Style"] = "Farbstil"
+L["Hover Color Style"] = "Hover Farbstil"
+L["Custom color"] = "Benutzerdefinierte Farbe"
+L["Hover Custom Color"] = "Hover Benutzerdefinierte Farbe"
+L["Position Settings"] = true
+L["The position parameters of the ElvUI instance difficulty symbol are used. To change the position, the position of the ElvUI symbol must be changed."] = "Es werden die Positionsparameter des Schwierigkeitssymbols der ElvUI-Instanz verwendet. Um die Position zu ändern, muss die Position des ElvUI-Symbols geändert werden."
+L["After setting the position, the interface must be reloaded once with /rl."] = "Nach dem Setzen der Position muss die Schnittstelle einmalig mit /rl neu geladen werden."
+L["Difficulties Colors"] = "Schwierigkeitsfarben"
+L["Other Colors"] = "Andere Farben"
+L["Instance Name"] = "Instanzname"
+L["Guild Group"] = "Gildengruppe"
+L["M+ Keystone Level"] = "M+ Schlüsselstein Level"
+L["Example"] = "Beispiel"
+
+--DataTexts--
+L["Datatext Colors"] = "Datentextfarbe"
+L["Combat Icon and Time"] = "Kampfsymbol und Zeit"
+L["M+ Score"] = "M+ Wertung"
+L["Teleports"] = true
+L["Professions"] = "Berufe"
+L["Dungeon"] = true
+L["Game Menu"] = "Spielmenü"
+L["Currency"] = "Währung"
+L["first and second Profession"] = "Erster und Zweiter Beruf"
+L["Durability and Item Level"] = "Haltbarkeit und Gegenstandsstufe"
+
+--Tags--
+L["Colors"] = "Farben"
+L["Icons"] = "Symbole"
+
+--Dock--
+
+--Castbar--
+
+--AFK-
+L["Info Screen"] = "Infobildschirm"
+L["Values"] = "Werte"
+L["Attributes"] = "Attribute"
+L["Enhancements"] = "Verbesserungen"
+L["Progress"] = "Fortschritt"
+L["Collect Garbage"] = "Müllsammlung"
+L["Attention! The path of the custom texture must comply with WoW standards. Example: Interface\\MYFOLDER\\MYFILE.tga. If you see only a green box, the path is not correct or there is a typo."] = "Achtung! Der Pfad der benutzerdefinierten Textur muss den WoW-Standards entsprechen. Beispiel: Interface\\MYFOLDER\\MYFILE.tga. Wenn Du nur ein grünes Kästchen siehst, ist der Pfad nicht korrekt oder es liegt ein Tippfehler vor."
+L["Optimal size for the logo is 512 x 128 Pixel"] = "Die optimale Größe für das Logo beträgt 512 x 128 Pixel"
+L["Logo Texture"] = "Logotextur"
+
+--About--
+L["Contact"] = "Kontakt"
+L["Licens"] = "Lizenz"
+L["Thanks to:"] = "Danke an:"
+
+--Changelog--
+L["Important:"] = "Wichtig:"
+L["New:"] = "Neu:"
+L["Update:"] = "Aktualisierung:"
+L["Fix:"] = "Behoben:"
---
+++ Addon/ElvUI_mMediaTag/localization/enUS.lua
@@ -0,0 +1,76 @@
+﻿local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "enUS", true, true)
+if not L then return end
+
+--Tree-
+L["General"] = true
+L["About"] = true
+L["Misc"] = true
+L["Castbar"] = true
+L["Changelog"] = true
+
+--General--
+L["Welcome text"] = true
+L["Roll Button"] = true
+L["Chat Button"] = true
+L["Keystone to Chat"] = true
+L["Show Keystone to Chat"] = true
+L["Instance Difficulty"] = true
+L["AFK Screen"] = true
+L["Show Welcome text"] = true
+L["Color Normal"] = true
+L["Color Hover"] = true
+L["Color Style"] = true
+L["Hover Color Style"] = true
+L["Custom color"] = true
+L["Hover Custom Color"] = true
+L["Position Settings"] = true
+L["The position parameters of the ElvUI instance difficulty symbol are used. To change the position, the position of the ElvUI symbol must be changed."] = true
+L["After setting the position, the interface must be reloaded once with /rl."] = true
+L["Difficulties Colors"] = true
+L["Other Colors"] = true
+L["Instance Name"] = true
+L["Guild Group"] = true
+L["M+ Keystone Level"] = true
+L["Example"] = true
+
+--DataTexts--
+L["Datatext Colors"] = true
+L["Combat Icon and Time"] = true
+L["M+ Score"] = true
+L["Teleports"] = true
+L["Professions"] = true
+L["Dungeon"] = true
+L["Game Menu"] = true
+L["Currency"] = true
+L["first and second Profession"] = true
+L["Durability and Item Level"] = true
+
+--Tags--
+L["Colors"] = true
+L["Icons"] = true
+
+--Dock--
+
+--Castbar--
+
+--AFK-
+L["Info Screen"] = true
+L["Values"] = true
+L["Attributes"] = true
+L["Enhancements"] = true
+L["Progress"] = true
+L["Collect Garbage"] = true
+L["Attention! The path of the custom texture must comply with WoW standards. Example: Interface\\MYFOLDER\\MYFILE.tga. If you see only a green box, the path is not correct or there is a typo."] = true
+L["Optimal size for the logo is 512 x 128 Pixel"] = true
+L["Logo Texture"] = true
+
+--About--
+L["Contact"] = true
+L["Licens"] = true
+L["Thanks to:"] = true
+
+--Changelog--
+L["Important:"] = true
+L["New:"] = true
+L["Update:"] = true
+L["Fix:"] = true
